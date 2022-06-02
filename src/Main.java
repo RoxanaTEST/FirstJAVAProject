e43a4f9a23f48dc9935ab7666763385ac04fa513
Roxana Pop <mihaela.roxana.pop@gmail.com>
2022-06-02 18:41:40 +0300

    Java tema lab 9

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,63 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello\n");
+        System.out.println("Roxana\n");
 
+        int z = 7;
+        int y = 8;
+        System.out.println( z + y );
+
+        double a = 20;
+        double b = 7;
+        System.out.println( a / b );
+
+        double c = -5;
+        double d = 8;
+        double e = 6;
+        System.out.println( c + d * e );
+
+
+        double f = 55;
+        double g = 9;
+        double h = ( f + g );
+        double i = ( h % g );
+        System.out.println( i );
+
+
+        int x1 = 20;
+        int x2 = -3;
+        int x3 = 5;
+        int x4 = 8;
+        System.out.println( x1 + x2 * x3 / x4 );
+
+
+        int a1,b1,c1,d1,e1;
+        a1 = 5;
+        b1 = 15;
+        c1 = 3;
+        d1 = 2;
+        e1 = 8;
+        System.out.println( a1 + b1 / c1 * d1 - e1 % c1 );
+
+
+
+
+        double m, n, o, p;
+        m = -5 + 8 * 6;
+        n = (55+9) % 9;
+        o = 20 + -3 * 5 / 8;
+        p = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
+
+
+
+        System.out.println("La steaua care-a rasarit\n" + "E o cale atat de lunga\n" + "Ca mii de ani i-au trebuit\n" + "Luminii sa ne-ajunga\n" +"\n" + "Poate de mult s-a stins în drum\n" + "În depărtări albastre,\n" + "Iar raza ei abia acum\n" + "Luci vederii noastre,");
+
+
+
+    }
 }
