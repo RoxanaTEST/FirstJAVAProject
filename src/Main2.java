777aa748ec658dde818079d8119ff14c27e15868
Roxana Pop <mihaela.roxana.pop@gmail.com>
2022-06-14 17:23:55 +0300

    lab 2 java

---
+++ src/Main2.java
@@ -0,0 +1,46 @@
+public class Main2 {
+    public static void main(String[] args) {
+        //exercitiul 1
+        System.out.println(String1.Printmyname());
+        System.out.println(String1.poezie());
+        System.out.println(Calculator.sum(7, 8));
+        System.out.println(Calculator.division(20, 7));
+        System.out.println(Calculator.exercise1(-5, 8, 6));
+        System.out.println(Calculator.exercise2(55, 9));
+        System.out.println(Calculator.exercise3(20, -3, 5, 8));
+            System.out.println( Calculator.exercise4(5, 15, 3, 2, 8));
+        //exeritiul 2
+        System.out.println("\n");
+        System.out.println(Calculator.sum2(99, 1));
+        System.out.println(Calculator.subtraction(2650, 49));
+        System.out.println(Calculator.multiply(33, 10));
+        System.out.println(Calculator.division2(100, 3));
+
+        System.out.println("\n");
+        //exercitiul 3   vezi clasa Drawing
+        System.out.println(
+                        "    j     a     v     v      a   \n"+
+                        "    j    a a     v   v      a a  \n"+
+                        " j  j   aaaaa     v v      aaaaa \n"+
+                        "  jj   a     a     v      a     a\n");
+
+        //exercitiul 4 Vezi clasa arithmetic
+
+        //exercitiul 5 Vezi clasa drawing
+        System.out.println(
+                        "   + ''''''''' +   \n"+
+                        " [ |   o   o   | ] \n"+
+                        "   |     ^     |   \n"+
+                        "   |           |   \n"+
+                        "   |   ' _ '   |   \n"+
+                        "   +___________+   \n");
+        //exercitiul 6
+        System.out.println("\n");
+        System.out.println(Calculator.divisionremainder(17,2));
+
+    }
+    //exercitiul 7 vezi temperature  class
+    //exercitiul 8 vezi distance class
+    //exercitiul 9
+}
+
