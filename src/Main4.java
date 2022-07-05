d1564235e47a7dcde8d4eaf6ea5eeca92f53fb07
Roxana Pop <mihaela.roxana.pop@gmail.com>
2022-07-05 16:40:14 +0300

    lab 4 java

---
+++ src/Main4.java
@@ -0,0 +1,54 @@
+public class Main4 {
+    public static void main(String[] args) {
+        //Tema web 4- FOR
+
+        LogicalOperators op = new LogicalOperators();
+
+        int x = 6;
+        op.Printtohundred(x);
+
+
+        float x1 = -160;
+        op.Printtohundred2(x1);
+
+
+
+        int number1 = 40;
+        int number2 = 20;
+        op.countBetweentwonumbers(number1,number2);
+
+
+
+        int nr1 = 25 ;
+        int nr2 = 8;
+        op.Printfromnr1tonr2(nr1,nr2);
+
+
+        op.displayEvenNo();
+
+
+        op.displayUnevenNo();
+
+
+        System.out.println(op.getSum(98));
+
+
+        System.out.println(op.getAverage(60));
+
+
+        op.printStars();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    }
+
