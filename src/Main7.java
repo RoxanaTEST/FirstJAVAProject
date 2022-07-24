d48f08e20a7a800fc0fc3bf1440a278d4c25837b
Roxana Pop <mihaela.roxana.pop@gmail.com>
2022-07-24 21:54:09 +0300

    lab 8

---
+++ src/Main7.java
@@ -0,0 +1,31 @@
+
+
+
+public class Main7 {
+
+    public static void main(String[] args)  {
+        Read read = new Read();
+        double x = read.getDouble();
+        System.out.println(x);
+
+        int y = read.getInt();
+        System.out.println(y);
+
+        float z = read.getFloat();
+        System.out.println(z);
+
+        long w = read.getLong();
+        System.out.println(w);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
